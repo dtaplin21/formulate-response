80c46b18631f0a034257322b4db1160e6983511a
Dom <dtaplin21@aol.com>
2023-07-20 20:01:09 -0700

    formulate

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// Your code here
+const http = require('http');
+
+const port = 5000
+
+
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"})
+
+   res.end("this is the response")
+})
+
+server.listen(port, () =>
+console.log(`server is rinning on ${port}`)
+)
